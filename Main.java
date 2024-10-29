fe57c9b3e7d66a5cdc3bd32317662367ada9364a
Abdullah-Al-Muaddi <Abdullah.AlMuadddi@gmail.com>
2024-10-30 01:40:27 +0300

    Update Main

--- Main.java
+++ Main.java
@@ -1,19 +1,22 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args) throws Exception{
+        String line="";
 
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("data\\dataset.csv")));
-        while(reader.readLine()!=null) {
-            StringTokenizer tokens = new StringTokenizer((String) reader.readLine(), ","); // this will read first line and separates values by (,) and stores them in tokens.
+        BufferedReader reader = new BufferedReader(new FileReader("data\\dataset.csv"));
+        while((line = reader.readLine())!=null) {
+            String[] data = line.split(",");
+            System.out.println(data[0]);
+            String[] words = data[1].toLowerCase().split(" ");//convert it to lowercase and then split it into words
 
-            // tokens.nextToken(); // this method will read the tokens values on each call.
-            System.out.println(tokens.nextToken());
-            reader.readLine();
+
+   
 }
 reader.close();
 
